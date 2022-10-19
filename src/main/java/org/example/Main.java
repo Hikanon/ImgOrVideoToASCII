54f6223bdc0b60ed61181520bd4247cb5c5cf503
Hikanon <roma.makhanov@mail.ru>
2022-10-19 22:37:41 +0300

    Help

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,39 +9,51 @@ import java.util.Objects;
 
 public class Main {
 
+    int glyphWidth = 11;
+    int glyphHeight = 23;
+
     public void convert(String fileName){
         BufferedImage img;
         try{
             img = ImageIO.read(Objects.requireNonNull(Main.class.getClassLoader().getResource(fileName)));
-            BufferedImage bwI = toBWI(img);
+            BufferedImage scaledBWImage = toScale(toBlackWhiteImage(img));
+            int charNumHor = (int) Math.round((double) img.getWidth()/glyphWidth);
+            int charNumVer = (int) Math.round((double) img.getHeight()/glyphHeight);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    BufferedImage toBWI(BufferedImage colorfulImage){
-        try {
-            BufferedImage bwImage = new BufferedImage(colorfulImage.getWidth(), colorfulImage.getHeight(), colorfulImage.getType());
-            for (int i = 0; i < colorfulImage.getWidth(); i++){
-                for(int j = 0; j < colorfulImage.getHeight(); j++){
-                    int r = new Color(colorfulImage.getRGB(i, j)).getRed();
-                    int g = new Color(colorfulImage.getRGB(i, j)).getGreen();
-                    int b = new Color(colorfulImage.getRGB(i, j)).getBlue();
-                    int color = (r+g+b)/3;
-                    if(color > 127){
-                        color = 255;
-                    }else{
-                        color = 0;
-                    }
-                    bwImage.setRGB(i, j, new Color(color, color, color).getRGB());
 
+    BufferedImage toScale(BufferedImage imageToScale) throws IOException {
+        int scaled = 2;
+        BufferedImage scaledImage = new BufferedImage(imageToScale.getWidth()/scaled , imageToScale.getHeight()/scaled, imageToScale.getType());
+        Graphics2D g2D = scaledImage.createGraphics();
+        g2D.drawImage(imageToScale, 0, 0, imageToScale.getWidth()/scaled, imageToScale.getHeight()/scaled, null);
+        g2D.dispose();
+        ImageIO.write(scaledImage, "png", new File("scaledBW.png"));
+        return scaledImage;
+    }
+
+    BufferedImage toBlackWhiteImage(BufferedImage colorfulImage) throws IOException {
+        BufferedImage bwImage = new BufferedImage(colorfulImage.getWidth(), colorfulImage.getHeight(), colorfulImage.getType());
+        for (int i = 0; i < colorfulImage.getWidth(); i++){
+            for(int j = 0; j < colorfulImage.getHeight(); j++){
+                int r = new Color(colorfulImage.getRGB(i, j)).getRed();
+                int g = new Color(colorfulImage.getRGB(i, j)).getGreen();
+                int b = new Color(colorfulImage.getRGB(i, j)).getBlue();
+                int color = (r+g+b)/3;
+                if(color > 127){
+                    color = 255;
+                }else{
+                    color = 0;
                 }
+                bwImage.setRGB(i, j, new Color(color, color, color).getRGB());
             }
-            ImageIO.write(bwImage ,"png" , new File("hehe.png"));
-            return bwImage;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+        ImageIO.write(bwImage ,"png" , new File("hehe.png"));
+        return bwImage;
     }
 
     public static void main(String[] args) {
